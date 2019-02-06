6a7e2f89afc8f70dae0985a787aefa0b6bcefc0d
johnsom4 <johnsom4@rose-hulman.edu>
2019-02-06 14:15:52 -0500

    Names in gui, and shared gui

--- src/rosebot.py
+++ src/rosebot.py
@@ -192,6 +192,12 @@ class ArmAndClaw(object):
 
     def raise_arm(self):
         """ Raises the Arm until its touch sensor is pressed. """
+        self.motor.turn_on(100)
+        while True:
+            if self.touch_sensor.is_pressed():
+                self.motor.turn_off()
+                break
+
 
     def calibrate_arm(self):
         """
@@ -202,7 +208,7 @@ class ArmAndClaw(object):
                (i.e., 14.2 motor revolutions),
           3. Resets the motor's position to 0.
         """
-
+    
     def move_arm_to_position(self, desired_arm_position):
         """
         Move its Arm to the given position, where 0 means all the way DOWN.
