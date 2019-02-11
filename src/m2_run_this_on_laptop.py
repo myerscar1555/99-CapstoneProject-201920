97b70df92265b1de48f81bcfa978c90635d6ee00
johnsom4 <johnsom4@rose-hulman.edu>
2019-02-11 13:56:46 -0500

    stuff

--- src/m2_run_this_on_laptop.py
+++ src/m2_run_this_on_laptop.py
@@ -3,7 +3,7 @@
   Displays the Graphical User Interface (GUI) and communicates with the robot.
 
   Authors:  Your professors (for the framework)
-    and Michael Johnson.
+    and Nasser Hegar.
   Winter term, 2018-2019.
 """
 
@@ -25,23 +25,24 @@ def main():
     mqtt_sender = com.MqttClient()
     mqtt_sender.connect_to_ev3()
 
+
     # -------------------------------------------------------------------------
     # The root TK object for the GUI:
     # -------------------------------------------------------------------------
     root = tkinter.Tk()
-    root.title('CSSE 120 Capstone Project Winter Quarter Freshman Year')
+    root.title("Robot Boi, Winter 2018-19")
+
 
     # -------------------------------------------------------------------------
     # The main frame, upon which the other frames are placed.
     # -------------------------------------------------------------------------
-    main_frame = ttk.Frame(root, padding=10, borderwidth=5, relief="groove")
+    main_frame = ttk.Frame(root,padding=10,borderwidth=5,relief="groove")
     main_frame.grid()
-
 
     # -------------------------------------------------------------------------
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
-    teleop_frame, arm_frame, control_frame = get_shared_frames(main_frame, mqtt_sender)
+    teleop_frame,arm_frame,control_frame,sensor_frame,soundsystem_frame = get_shared_frames(main_frame, mqtt_sender)
 
 
     # -------------------------------------------------------------------------
@@ -52,26 +53,32 @@ def main():
     # -------------------------------------------------------------------------
     # Grid the frames.
     # -------------------------------------------------------------------------
-    grid_frames(teleop_frame, arm_frame, control_frame)
-
+    grid_frames(teleop_frame,arm_frame,control_frame,sensor_frame,soundsystem_frame)
 
     # -------------------------------------------------------------------------
     # The event loop:
     # -------------------------------------------------------------------------
+
     root.mainloop()
 
+
+
 def get_shared_frames(main_frame, mqtt_sender):
-    teleop_frame = shared_gui.get_teleoperation_frame(main_frame, mqtt_sender)
-    arm_frame = shared_gui.get_arm_frame(main_frame, mqtt_sender)
-    control_arm = shared_gui.get_control_frame(main_frame, mqtt_sender)
+    teleop_frame = shared_gui.get_teleoperation_frame(main_frame,mqtt_sender)
+    arm_frame = shared_gui.get_arm_frame(main_frame,mqtt_sender)
+    control_arm = shared_gui.get_control_frame(main_frame,mqtt_sender)
+    sensor_frame = shared_gui.get_sensor_frame(main_frame,mqtt_sender)
+    soundsystem_frame = shared_gui.get_soundsystem_frame(main_frame,mqtt_sender)
 
-    return teleop_frame, arm_frame, control_arm
+    return teleop_frame,arm_frame,control_arm,sensor_frame,soundsystem_frame
 
 
-def grid_frames(teleop_frame, arm_frame, control_frame):
-    teleop_frame.grid(row=0, column=0)
-    arm_frame.grid(row=1, column=0)
-    control_frame.grid(row=2, column=0)
+def grid_frames(teleop_frame, arm_frame, control_frame,sensor_frame, soundsystem_frame):
+    teleop_frame.grid(row=0,column=0)
+    arm_frame.grid(row=1,column=0)
+    control_frame.grid(row=2,column=0)
+    sensor_frame.grid(row=3,column=0)
+    soundsystem_frame.grid(row=4,column=0)
 
 
 # -----------------------------------------------------------------------------
