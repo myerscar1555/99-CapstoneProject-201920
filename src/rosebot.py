8b9c057a9664acc73f19bb222de00fb10fdc2b19
myerscar1555 <myerscl@rose-hulman.edu>
2019-02-14 15:08:57 -0500

    m1 stuff

--- src/rosebot.py
+++ src/rosebot.py
@@ -303,6 +303,12 @@ class DriveSystem(object):
         of the trained color whose area is at least the given area.
         Requires that the user train the camera on the color of the object.
         """
+        while True:
+            b = self.sensor_system.camera.get_biggest_blob()
+            self.right_motor.turn_on((-1) * speed)
+            self.left_motor.turn_on(speed)
+            if (b.width * b.height) >= area:
+                
 
     def spin_counterclockwise_until_sees_object(self, speed, area):
         """
