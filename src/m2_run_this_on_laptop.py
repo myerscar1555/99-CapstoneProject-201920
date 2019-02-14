ea6af63eeb131bf9114550261ce4d9261a811c0c
johnsom4 <johnsom4@rose-hulman.edu>
2019-02-14 15:05:23 -0500

    stuff

--- src/m2_run_this_on_laptop.py
+++ src/m2_run_this_on_laptop.py
@@ -42,7 +42,7 @@ def main():
     # -------------------------------------------------------------------------
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
-    teleop_frame,arm_frame,control_frame,sensor_frame,soundsystem_frame = get_shared_frames(main_frame, mqtt_sender)
+    teleop_frame,arm_frame,control_frame,sensor_frame,soundsystem_frame, infrared_frame = get_shared_frames(main_frame, mqtt_sender)
 
 
     # -------------------------------------------------------------------------
@@ -53,7 +53,7 @@ def main():
     # -------------------------------------------------------------------------
     # Grid the frames.
     # -------------------------------------------------------------------------
-    grid_frames(teleop_frame,arm_frame,control_frame,sensor_frame,soundsystem_frame)
+    grid_frames(teleop_frame,arm_frame,control_frame,sensor_frame,soundsystem_frame, infrared_frame)
 
     # -------------------------------------------------------------------------
     # The event loop:
@@ -69,16 +69,55 @@ def get_shared_frames(main_frame, mqtt_sender):
     control_arm = shared_gui.get_control_frame(main_frame,mqtt_sender)
     sensor_frame = shared_gui.get_sensor_frame(main_frame,mqtt_sender)
     soundsystem_frame = shared_gui.get_soundsystem_frame(main_frame,mqtt_sender)
+    infrared_frame = get_m2_personal_infrared_frame(main_frame, mqtt_sender)
 
-    return teleop_frame,arm_frame,control_arm,sensor_frame,soundsystem_frame
+    return teleop_frame,arm_frame,control_arm,sensor_frame,soundsystem_frame, infrared_frame
+
+def get_m2_personal_infrared_frame(main_frame,mqtt_sender):
+    frame = ttk.Frame(main_frame, padding=5, borderwidth=5, relief="ridge")
+    frame.grid()
+
+    # Construct the widgets on the frame:
+    frame_label = ttk.Label(frame, text="m1 Personal")
+    beep_according_to_distance = ttk.Button(frame, text="Frequency raises at rate based on how far from object")
+    beep_label = ttk.Label(frame, text="Initial Frequency")
+    frequency_entry = ttk.Entry(frame, width=8)
+    rate_label = ttk.Label(frame, text="Rate of Increase")
+    rate_entry = ttk.Entry(frame, width=8)
+    speed_label = ttk.Label(frame, text='Speed of robot')
+    speed_slider = ttk.Scale(frame, to=10)
+    distance_label = ttk.Label(frame, text='Distance to stop')
+    distance_entry = ttk.Entry(frame, width=8)
 
 
-def grid_frames(teleop_frame, arm_frame, control_frame,sensor_frame, soundsystem_frame):
+    # Grid the widgets:
+    frame_label.grid(row=1, column=1)
+    beep_according_to_distance.grid(row=2, column=0)
+    beep_label.grid(row=3, column=0)
+    rate_entry.grid(row=4, column=1)
+    rate_label.grid(row=4, column=0)
+    frequency_entry.grid(row=3, column=1)
+    speed_label.grid(row=6, column=0)
+    speed_slider.grid(row=6, column=1)
+    distance_label.grid(row=7, column=0)
+    distance_entry.grid(row=7, column=1)
+
+    # Set the Button callbacks:
+    beep_according_to_distance["command"] = lambda: handle_tone_until_distance_is_less_than(mqtt_sender, distance_entry, speed_slider, frequency_entry, rate_entry)
+
+    return frame
+
+def handle_tone_until_distance_is_less_than(mqtt_sender,distance_entry, speed_entry, frequency_entry, rate_entry):
+    print("will go until closer than: ", distance_entry.get())
+    mqtt_sender.send_message('go_tone_until_distance_is_less_than', [distance_entry.get(), speed_entry.get(), frequency_entry.get, rate_entry.get])
+
+def grid_frames(teleop_frame, arm_frame, control_frame,sensor_frame, soundsystem_frame, infrared_frame):
     teleop_frame.grid(row=0,column=0)
     arm_frame.grid(row=1,column=0)
     control_frame.grid(row=2,column=0)
     sensor_frame.grid(row=3,column=0)
     soundsystem_frame.grid(row=4,column=0)
+    infrared_frame.grid(row=1, column=1)
 
 
 # -----------------------------------------------------------------------------
