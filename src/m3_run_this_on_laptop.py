ba7acf7d247ae14d844207fec54e359aef517fb4
hegarnn <hegarnn@rose-hulman.edu>
2019-02-13 22:51:37 -0500

    Nasser

--- src/m3_run_this_on_laptop.py
+++ src/m3_run_this_on_laptop.py
@@ -42,7 +42,7 @@ def main():
     # -------------------------------------------------------------------------
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
-    teleop_frame,arm_frame,control_frame,sensor_frame,soundsystem_frame,color_frame = get_shared_frames(main_frame, mqtt_sender)
+    teleop_frame,arm_frame,control_frame,sensor_frame,soundsystem_frame,color_frame,infrared_frame = get_shared_frames(main_frame, mqtt_sender)
 
 
     # -------------------------------------------------------------------------
@@ -53,7 +53,7 @@ def main():
     # -------------------------------------------------------------------------
     # Grid the frames.
     # -------------------------------------------------------------------------
-    grid_shared_frames(teleop_frame,arm_frame,control_frame,sensor_frame,soundsystem_frame,color_frame)
+    grid_shared_frames(teleop_frame,arm_frame,control_frame,sensor_frame,soundsystem_frame,color_frame,infrared_frame)
 
     # -------------------------------------------------------------------------
     # The event loop:
@@ -81,17 +81,19 @@ def get_shared_frames(main_frame, mqtt_sender):
     sensor_frame = shared_gui.get_sensor_frame(main_frame,mqtt_sender)
     soundsystem_frame = shared_gui.get_soundsystem_frame(main_frame,mqtt_sender)
     color_frame = shared_gui.get_color_sensor_frame(main_frame,mqtt_sender)
+    infrared_frame = shared_gui.get_infrared_proximity_sensor_frame(main_frame,mqtt_sender)
 
-    return teleop_frame,arm_frame,control_arm,sensor_frame,soundsystem_frame,color_frame
+    return teleop_frame,arm_frame,control_arm,sensor_frame,soundsystem_frame,color_frame,infrared_frame
 
 
-def grid_shared_frames(teleop_frame, arm_frame, control_frame,sensor_frame, soundsystem_frame,color_frame):
+def grid_shared_frames(teleop_frame, arm_frame, control_frame,sensor_frame, soundsystem_frame,color_frame, infrared_frame):
     teleop_frame.grid(row=0,column=0)
     arm_frame.grid(row=1,column=0)
     control_frame.grid(row=2,column=0)
     sensor_frame.grid(row=3,column=0)
     soundsystem_frame.grid(row=4,column=0)
     color_frame.grid(row=0, column=1)
+    infrared_frame.grid(row=1, column=1)
 
 
 # -----------------------------------------------------------------------------
