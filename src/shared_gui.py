a44105c40aba8e4062fa4e15dc43f28d6b6c66b8
myerscar1555 <myerscl@rose-hulman.edu>
2019-02-20 12:51:04 -0500

    m1 stuff

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -574,6 +574,28 @@ def get_ask_for_food_frame(window,mqtt_sender):
 
     return frame
 
+def get_petting_frame(window,mqtt_sender):
+    frame = ttk.Frame(window, padding=5, borderwidth=5, relief="ridge")
+    frame.grid()
+
+    # Construct the widgets on the frame:
+    frame_label = ttk.Label(frame, text="Get Pet")
+    get_pet = ttk.Button(frame, text="Meows at door until you let it out")
+    number_label = ttk.Label(frame, text="Times to get pet before running away")
+    number_entry = ttk.Entry(frame, width=8)
+
+
+    # Grid the widgets:
+    frame_label.grid(row=1, column=1)
+    get_pet.grid(row=2, column=1)
+    number_entry.grid(row=3, column=0)
+    number_label.grid(row=4, column=0)
+
+    # Set the Button callbacks:
+    get_pet["command"] = lambda: handle_get_pet(mqtt_sender, number_entry)
+
+    return frame
+
 def handle_tone_until_distance_is_less_than(mqtt_sender,distance_entry, speed_entry, frequency_entry, rate_entry):
     print("will go until closer than: ", distance_entry.get())
     mqtt_sender.send_message('tone_until_distance_is_less_than', [distance_entry.get(), speed_entry.get(), frequency_entry.get(), rate_entry.get()])
